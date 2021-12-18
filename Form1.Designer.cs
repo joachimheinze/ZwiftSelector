28de7a98356f0f8094ed79620bdb37891619781f
Joachim Heinze <jheinze@ib-heinze.com>
2021-12-18 14:19:17 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZwiftSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zwiftselection = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aktWorld = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Zwiftselection
+            // 
+            this.Zwiftselection.FormattingEnabled = true;
+            this.Zwiftselection.Items.AddRange(new object[] {
+            "Watopia",
+            "France",
+            "Makuri Islands",
+            "Innsbruck",
+            "Richmond",
+            "New York",
+            "London",
+            "Yorkshire",
+            "Paris"});
+            this.Zwiftselection.Location = new System.Drawing.Point(119, 31);
+            this.Zwiftselection.Name = "Zwiftselection";
+            this.Zwiftselection.Size = new System.Drawing.Size(232, 21);
+            this.Zwiftselection.TabIndex = 0;
+            this.Zwiftselection.SelectedIndexChanged += new System.EventHandler(this.Zwiftselection_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "World";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "aktuell eingestellt";
+            // 
+            // aktWorld
+            // 
+            this.aktWorld.Enabled = false;
+            this.aktWorld.Location = new System.Drawing.Point(119, 5);
+            this.aktWorld.Name = "aktWorld";
+            this.aktWorld.Size = new System.Drawing.Size(232, 20);
+            this.aktWorld.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 58);
+            this.Controls.Add(this.aktWorld);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Zwiftselection);
+            this.Name = "Form1";
+            this.Text = "Zwift Selector (C) 2021 J. Heinze";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Zwiftselection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox aktWorld;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+
+namespace ZwiftSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            XmlDocument doc = new XmlDocument();
+            String xmlFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Zwift\prefs.xml";
+            doc.Load(xmlFile);
+            XmlNodeList nodeList;
+            XmlNode root = doc.DocumentElement;
+            XmlElement node = (XmlElement)doc.SelectSingleNode("//WORLD");
+            if (node != null)
+            {
+                setWorld(node.InnerText);
+            }
+
+
+
+        } 
+
+        private void setWorld (String wid)
+        {
+            switch (wid)
+            {
+                case "1":
+                    aktWorld.Text = "Watopia";
+                    break;
+                case "2":
+                    aktWorld.Text = "Richmond";
+                    break;
+                case "3":
+                    aktWorld.Text = "London";
+                    break;
+                case "4":
+                    aktWorld.Text = "New York";
+                    break;
+                case "5":
+                    aktWorld.Text = "Insbruck";
+                    break;
+                case "7":
+                    aktWorld.Text = "Yorkshire";
+                    break;
+                case "9":
+                    aktWorld.Text = "Makuri Islands";
+                    break;
+                case "10":
+                    aktWorld.Text = "France";
+                    break;
+                case "11":
+                    aktWorld.Text = "Paris";
+                    break;
+                default:
+                    aktWorld.Text = "Watopia";
+                    break;
+            }
+
+
+        }
+
+        private void Zwiftselection_SelectedIndexChanged(object sender, EventArgs e)
+        { int selection = 0;
+            switch (Zwiftselection.SelectedItem)
+            {
+                case "Watopia":
+                    selection = 1;
+                    break;
+                case "Richmond":
+                    selection = 2;
+                    break;
+                case "London":
+                    selection = 3;
+                    break;
+                case "New York":
+                    selection = 4;
+                    break;
+                case "Innsbruck":
+                    selection = 5;
+                    break;
+                case "Yorkshire":
+                    selection = 7;
+                    break;
+                case "Makuri Islands":
+                    selection = 9;
+                    break;
+                case "France":
+                    selection = 10;
+                    break;
+                case "Paris":
+                    selection = 11;
+                    break;
+                default:
+                    selection = 0;
+                    break;
+            }
+            XmlDocument doc = new XmlDocument();
+            String xmlFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Zwift\prefs.xml";
+            doc.Load(xmlFile);
+            XmlNodeList nodeList;
+            XmlNode root = doc.DocumentElement;
+            XmlElement node = (XmlElement)doc.SelectSingleNode("/ZWIFT/WORLD");
+            if (node !=  null)
+            {
+                node.InnerText = selection.ToString();
+
+            }
+            else
+            {
+                XmlNode root1 = doc.DocumentElement;
+
+                //Create a new node.
+                XmlElement elem = doc.CreateElement("WORLD");
+                elem.InnerText = selection.ToString();
+
+                //Add the node to the document.
+                root.InsertBefore(elem, root.FirstChild);
+
+                //Console.WriteLine("Display the modified XML...");
+                //doc.Save(Console.Out);
+            }
+
+            try
+            {
+                 doc.Save(xmlFile);
+           }
+            catch (UnauthorizedAccessException)
+            {
+                FileAttributes attr = (new FileInfo(xmlFile)).Attributes;
+                Console.Write("UnAuthorizedAccessException: Unable to access file. ");
+                if ((attr & FileAttributes.ReadOnly) > 0)
+                    Console.Write("The file is read-only.");
+                String xmlFile1 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Zwift\prefs1.xml";
+                doc.Save(xmlFile1);
+                File.Delete(xmlFile);
+                File.Copy(xmlFile1, xmlFile);
+
+
+            }
+            finally
+            {
+               setWorld(selection.ToString()   );
+            }
+
+        }
+    }
+}
